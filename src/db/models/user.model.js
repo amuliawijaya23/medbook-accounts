97ca91654a7431d7a79fe7fd0cf921bc34f8741a
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-14 16:51:40 +0700

    removes user medical_records from db selection on findUser

--- src/db/models/user.model.js
+++ src/db/models/user.model.js
@@ -12,7 +12,8 @@ const userSchema = new Schema({
           frequency: { type: String, required: true }
         }
       ],
-      required: true
+      required: true,
+      select: false
     }
   }
 });
