4ac44a871c2a7a8d5d117318c18919902095b204
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-14 22:34:08 +0700

    adds new client strategy for passport

--- src/db/controllers/auth.controller.js
+++ src/db/controllers/auth.controller.js
@@ -1,27 +1,50 @@
 import passport from 'passport';
 import { BasicStrategy } from 'passport-http';
 import { getUserByEmail } from '../models/user.model.js';
+import { getClientbyClientId } from '../models/code.model.js';
 
 import bcrypt from 'bcrypt';
 
 passport.use(
-  new BasicStrategy(async (email, password, cb) => {
+  new BasicStrategy(async (email, password, done) => {
     try {
-      if (!email || !password) return cb(null, false);
+      if (!email || !password) return done(null, false);
 
       const user = await getUserByEmail(email).select('+password');
 
-      if (!user) return cb(null, false);
+      if (!user) return done(null, false);
 
       const isMatch = bcrypt.compareSync(password, user.password);
 
-      if (!isMatch) return cb(null, false);
+      if (!isMatch) return done(null, false);
 
-      return cb(null, user);
+      return done(null, user);
     } catch (error) {
-      console.log(error);
+      return done(error);
+    }
+  })
+);
+
+passport.use(
+  'client-basic',
+  new BasicStrategy(async (clientId, clientSecret, done) => {
+    try {
+      if (!clientId || !clientSecret) return done(null, false);
+
+      const client = await getClientbyClientId(clientId).select('+clientSecret');
+
+      if (!client) return done(null, false);
+
+      const isMatch = bcrypt.compareSync(clientSecret, client.clientSecret);
+
+      if (!isMatch) return done(null, false);
+
+      return done(null, client);
+    } catch (error) {
+      return done(error);
     }
   })
 );
 
 export const isAuthenticated = passport.authenticate('basic', { session: false });
+export const isClientAuthenticated = passport.authenticate('client-basic', { session: false });
--- src/db/models/client.model.js
+++ src/db/models/client.model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 const clientSchema = new Schema({
   clientId: { type: String, required: true },
-  clientSecret: { type: String, required: true }
+  clientSecret: { type: String, required: true, select: false }
 });
 
 export const Client = mongoose.model.Client || mongoose.model('Client', clientSchema);
